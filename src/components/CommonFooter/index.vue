783b08368efc2e34ea70a00318fbbfdd45807d90
xiaoyang <michstabe515@163.com>
2024-09-19 09:28:47 +0800

    fix: 修改备案号

--- src/components/CommonFooter/index.vue
+++ src/components/CommonFooter/index.vue
@@ -13,7 +13,7 @@ const technologyList = [
 
 <template>
   <div class="common-footer">
-    <div class="item mb-16">MichstaBe | 个人博客 | 蒙ICP备2021004700号</div>
+    <div class="item mb-16">MichstaBe | 个人博客 | ICP 备案号: <a style="color: var(--base-text-color-white);" href="https://beian.miit.gov.cn/" target="_blank">蒙ICP备2021004700号-1</a></div>
 
     <div class="item mb-16">
       参考：
